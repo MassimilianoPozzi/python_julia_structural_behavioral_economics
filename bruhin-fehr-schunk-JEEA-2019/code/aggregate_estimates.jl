1c922000f47187f296f19fdf04f1e1a630e010f7
MassimilianoPozzi <massimiliano.pozzi07@gmail.com>
2021-11-16 12:42:31 +0100

    Update aggregate_estimates.jl
    Now the notebook works also on Pluto, as well as Neptune. Added Hypothesis testing and JuMP for optimization. Updated the output table

--- bruhin-fehr-schunk-JEEA-2019/code/aggregate_estimates.jl
+++ bruhin-fehr-schunk-JEEA-2019/code/aggregate_estimates.jl
@@ -1,11 +1,13 @@
 ### A Pluto.jl notebook ###
-# v0.14.0
+# v0.14.7
 
 using Markdown
 using InteractiveUtils
 
 # ╔═╡ a0736b60-3c7d-11ec-1b71-27fd38cbf6e4
-# Import packages used
+begin
+
+# Import packages used. We load both Optim and JuMP to compare how they work.
 
 using Distributions
 using DelimitedFiles
@@ -18,6 +20,10 @@ using Statistics
 using FiniteDiff
 using LinearAlgebra
 using ForwardDiff
+using JuMP 
+import Ipopt
+	
+end
 
 # ╔═╡ 7b7526d0-3c9d-11ec-3575-37a66c520386
 # The code in this notebook performs the aggregate estimates (table 1) for the model in the paper 'The Many Faces of Human Sociality: Uncovering the Distribution and Stability 
@@ -28,34 +34,41 @@ using ForwardDiff
 # Massimiliano Pozzi (pozzi.massimiliano@studbocconi.it)
 # Salvatore Nunnari (salvatore.nunnari@unibocconi.it)
 
+# ╔═╡ c7fb5b79-7d87-49c5-9459-a360e1057715
+# Starting and ending each cell with begin/end is necessary when using Pluto but not when using Neptune
+
 # ╔═╡ e0965590-3c7d-11ec-13b6-a7d15db3fd9f
+begin
+
 # 1. Data cleaning and data preparation
 
-# We import the relevant dataset containing the data on the 39 dictator games and 78 reciprocity games in Session 1. We then remove from the dataset those subjects that 
-# behaved very inconsistenly throughout the games. These subjects are identified from the individual estimates that we do not run in this notebook. 
-# dropped_subjects_section4paragraph2.csv contains the id of these subjects.
+# We import the relevant dataset containing the data on the 39 dictator games and 78 reciprocity games in Session 1. We then remove from the dataset those subjects that behaved very inconsistenly throughout the games. These subjects are identified from the individual estimates that we do not run in this notebook. dropped_subjects_section4paragraph2.csv contains the id of these subjects.
 
-# Load the data on Session 1
+# Load the data on Session 1 and create a nice DataFrame
 data, header = readdlm(raw"../input/choices_exp1.csv",',',header=true)
 dt1 = DataFrame(data, vec(header))
 
 # Load dropped_subjects_section4paragraph2.csv 
 data_drop = readdlm(raw"../input/dropped_subjects_section4paragraph2.csv",',',header=false)
+	
+# drop the session 1 subjects whose IDs are listed in the data_drop dataframe (14 individuals)
 
-# drop the subjects whose ids are listed in the data_drop dataframe (14 individuals)
-for i=1:length(data_drop)
-	global dt1=dt1[dt1.sid .!= data_drop[i],:] # need global else variables inside the loop can’t access variables outside the loop
-end;
+dt1 = dt1[[i for i=1:length(dt1.sid) if !(dt1.sid[i] in(data_drop))],:]
+	
+end
 
 # ╔═╡ d8928282-3c85-11ec-00ca-c5215c731b33
+begin
+
 # Create indicators_x and indicators_y. These are the columns s, r, q, v
 
 indicators_x = hcat(dt1.s_x,dt1.r_x,dt1.q,dt1.v)
-indicators_y = hcat(dt1.s_y,dt1.r_y,dt1.q,dt1.v);
+indicators_y = hcat(dt1.s_y,dt1.r_y,dt1.q,dt1.v)
+	
+end;
 
 # ╔═╡ 56a26400-422c-11ec-3599-77a4d3d0e180
-# Define the function to minimize. This is the negative of the log likelihood of observing our data given the parameters of the model. For more information refer to equation
-# (4) and (5) in the paper
+# Define the function to minimize. This is the negative of the log likelihood of observing our data given the parameters of the model. For more information refer to equation (4) and (5) in the paper or the python notebook
 
 # v is the vector of parameters (θ,σ)
 # y is the choice of the player
@@ -71,6 +84,7 @@ function loglikeM(v,y,self_x, other_x, self_y,other_y,indicators_x,indicators_y)
     rli = (indicators_y * beta)
 	uleft  = sigma.*( (1 .-lli) .*self_x .+lli .*other_x) # utility when choosing allocation X (left)
 	uright = sigma.*( (1 .-rli) .*self_y .+rli .*other_y) # utility when choosing 	allocation Y (right)
+	
 	# probs is a vector 1x18720 containing the likelihood of observing the data of a single game
 	probs  = (exp.(uleft)./(exp.(uleft).+exp.(uright))).^y .* (exp.(uright)./
 		(exp.(uleft).+exp.(uright))).^(1 .-y)
@@ -79,11 +93,15 @@ function loglikeM(v,y,self_x, other_x, self_y,other_y,indicators_x,indicators_y)
 	end
 
 # ╔═╡ dfcceb10-3c7d-11ec-2664-9d36301add75
+begin
+
 # initialize random starting guesses. Same random initial guesses as in the R code
 
 beta_init = rand(Uniform(0.01,0.02),4) # close to zero for α, β, γ, δ
 sigma_init = log.(rand(Uniform(0.05,0.8),1)/mean([mean(dt1.self_x),mean(dt1.other_x),mean(dt1.self_y),mean(dt1.other_y)])) # log since in function we take exp
 v0 = [beta_init;sigma_init]
+	
+end
 
 # ╔═╡ a3f76eb0-3d51-11ec-20e7-0d245610572b
 # Wrap our function. Optim.optimize can only take one argument
@@ -94,23 +112,23 @@ function ll(v)
 end
 
 # ╔═╡ 1baa58c0-4494-11ec-1794-6b4d74f6812b
-# This cell computes the aggregate estimate for session 1 using the BFGS algortihm. We do not provide the analytical gradient since the one computed numerically by the 
-# algorithm is precise enough for this problem
+begin
 
-sol = optimize(ll,v0,method=BFGS(),iterations = 10000,store_trace=true, extended_trace=true)
+# This cell computes the aggregate estimate for session 1 using the BFGS algortihm. We do not provide the analytical gradient since the one computed numerically by the algorithm is precise enough for this problem
+
+sol = optimize(ll,v0,method=BFGS(),iterations = 10000,store_trace=true, extended_trace=true, autodiff=:forward)
 res_s1 = Optim.minimizer(sol) # estimates
 results_s1 = [res_s1[1:4];exp(res_s1[5])] # Need to take exp for sigma
-vloglike_s1 = Optim.minimum(sol) # Value of loglikelihood evaluated in the minimum
+vloglike_s1 = -Optim.minimum(sol) # Value of loglikelihood evaluated in the minimum
+
+end
 
 # ╔═╡ f576544e-3d55-11ec-009e-5ddcc502108b
 # Compute the individual cluster robust standard errors. The formula is the following:
 
 # adj * (inv_hessian * grad_con * inv_hessian). 
 
-# adj is a correction for the degree of freedoms and the number of clusters.
-# inv_hessian is the inverse of the hessian of the negative log-likelihood evaluated at the minimum.
-# grad_con is is a 5x5 matrix of gradient contributions. It is computed as the sum over all clusters of the outer product of the sum of the single observation gradient 
-# evaluated in the minimum for all observations of an individual cluster.
+# adj is a correction for the degree of freedoms and the number of clusters. inv_hessian is the inverse of the hessian of the negative log-likelihood evaluated at the minimum. grad_con is is a 5x5 matrix of gradient contributions. It is computed as the sum over all clusters of the outer product of the sum of the single observation gradient evaluated in the minimum for all observations of an individual cluster.
 
 
 # Define the function that computes the matrix of individual gradient contribution 
@@ -156,12 +174,13 @@ function congradloglike(v,y,self_x,other_x,self_y,other_y,indicators_x,indicator
 end
 
 # ╔═╡ f558e140-3d55-11ec-075d-9d31f531f525
+begin
+
 # Compute the cluster robust standard errors. 
 
-# This would be the code to use the inverse of the hessian stored in the BFGS algorithm. This is less precise than the one computed with ForwardDiff, so we don't use it but we 
-# leave the code since it could be interesting to know.
+# This is the code to use the inverse of the hessian stored in the BFGS algorithm. We use the one computed using the Pkg ForwardDiff but they are basically the same 
 
-# inverse_hessian_s1 = sol.trace[end].metadata["~inv(H)"] 
+inverse_hessian_s1 = sol.trace[end].metadata["~inv(H)"] 
 
 
 # Compute the hessian numerically using the ForwardDiff package 
@@ -179,26 +198,36 @@ se_s1 = sqrt.(diag(varcov_s1))
 se_s1 = [se_s1[1:4]; sqrt(exp(res_s1[5])^2*se_s1[5]^2)]
 
 # sqrt(exp(res_s1[5])^2*se_s1[5]^2) is the formula to go from standard error log(sigma) to standard error of sigma. We use the Delta method
+	
+end
 
 # ╔═╡ f970b080-3cb2-11ec-0e09-65acfacc9837
 # Compute aggregate estimates for session 2
 
 # ╔═╡ 9e5d90a0-3cb2-11ec-2efc-8189c078aee7
+begin
+
 # Load the data for Session 2 and remove the dropped subjects as before
 
-data, header = readdlm(raw"../input/choices_exp2.csv",',',header=true)
-dt2 = DataFrame(data, vec(header))
-for i=1:length(data_drop)
-	global dt2=dt2[dt2.sid .!= data_drop[i],:] # need global else variables inside the loop can’t access variables outside the loop
-end;
+data2, header2 = readdlm(raw"../input/choices_exp2.csv",',',header=true)
+dt2 = DataFrame(data2, vec(header2))
+dt2 = dt2[[i for i=1:length(dt2.sid) if !(dt2.sid[i] in(data_drop))],:]
+	
+end
 
 # ╔═╡ e6ab8b50-3cb2-11ec-0b39-e5424b4fdbd2
+begin
+
 # Create indicators_x and indicators_y. These are the columns s, r, q, v
 
 indicators_x2 = hcat(dt2.s_x,dt2.r_x,dt2.q,dt2.v)
-indicators_y2 = hcat(dt2.s_y,dt2.r_y,dt2.q,dt2.v);
+indicators_y2 = hcat(dt2.s_y,dt2.r_y,dt2.q,dt2.v)
+
+end;
 
 # ╔═╡ d554ea40-3cb2-11ec-1642-99c3aadfd3e4
+begin
+
 # This cell computes the aggregate estimate for session 2
 
 function ll2(v)
@@ -206,35 +235,129 @@ function ll2(v)
 	return fll
 end
 
-sol = optimize(ll2,v0,method=BFGS(),iterations = 10000,store_trace=true, extended_trace=true)
-res_s2 = Optim.minimizer(sol)
+sol2 = optimize(ll2,v0,method=BFGS(),iterations = 10000,store_trace=true, extended_trace=true, autodiff=:forward)
+res_s2 = Optim.minimizer(sol2)
 results_s2 = [res_s2[1:4];exp(res_s2[5])]
-vloglike_s2 = Optim.minimum(sol)
+vloglike_s2 = -Optim.minimum(sol)
+	
+end
 
 # ╔═╡ 586df880-3cd7-11ec-348c-6133b9ddba73
+begin
+
 # Compute the cluster robust standard errors. 
 
 inv_hessian_num2 = inv(ForwardDiff.hessian(ll2,res_s2))
 
-adj = (length(dt2.self_x)-1) / (length(dt2.self_x)-length(res_s2)) * (160/159) # degree of freedom adjustment
+adj2 = (length(dt2.self_x)-1) / (length(dt2.self_x)-length(res_s2)) * (160/159) # degree of freedom adjustment
 
-grad_contribution = congradloglike(res_s2,dt2.choice_x,dt2.self_x,dt2.other_x,dt2.self_y,dt2.other_y,indicators_x2,indicators_y2,dt2.sid)
+grad_contribution2 = congradloglike(res_s2,dt2.choice_x,dt2.self_x,dt2.other_x,dt2.self_y,dt2.other_y,indicators_x2,indicators_y2,dt2.sid)
 
 # Sandwich formula for the clustered standard errors
 
-varcov_s2 = adj .* (inv_hessian_num2 * grad_contribution * inv_hessian_num2)
+varcov_s2 = adj2 .* (inv_hessian_num2 * grad_contribution2 * inv_hessian_num2)
 se_s2 = sqrt.(diag(varcov_s2))
 se_s2 = [se_s2[1:4]; sqrt(exp(res_s2[5])^2*se_s2[5]^2)]
+	
+end
+
+# ╔═╡ c32cedfe-daf7-4b0b-81bc-e8028a050294
+begin
+
+# Hypothesis testing
+
+# We now do some hypothesis testing on the parameters we obtained in session 1 and session 2. We first compute the z-test statistics and the corresponding p-values to check if each parameter we obtained is statistically different from zero. We then compute the p-value of a z-test to check if the parameters we obtained in session 1 are statistically different from the parameters we obtained in session 2.
+
+# Compute the z-test statistics and the corresponding p-values to check if the parameters are statistically different from zero
+
+# Session 1
+
+zvalues_s1 = results_s1 ./ se_s1
+pvalues_s1 = 2*(1 .-cdf.(Normal(0,1),zvalues_s1))
+
+# Session 2
+
+zvalues_s2 = results_s2 ./ se_s2
+pvalues_s2 = 2*(1 .-cdf.(Normal(0,1),zvalues_s2))
+
+# Check if parameters obtained in session 1 are statistically different from parameters in session 2
+
+# First we need the variance for the parameters in session 1 and session2
+
+var_s1 = se_s1.^2
+var_s2 = se_s2.^2
+
+# Now we compute the p-values of the z-test statistics 
+
+zvalues_s1s2 = abs.(results_s1.-results_s2) ./ sqrt.(var_s1.+var_s2)
+pvalues_s1s2 = 2*(1 .-cdf.(Normal(0,1),zvalues_s1s2))
+	
+end
 
 # ╔═╡ 802c89c0-3d60-11ec-1d69-0dcf94880bf3
-# Save the results in a csv file
+begin
+
+# Create a new table and save the results in a csv file. We round results up to the third decimal
+
+parameters_name = ["α: Weight on other's payoff when behind",
+                   "β: Weight on other's payoff when ahead",
+                   "γ: Measure of positive reciprocity",
+                   "δ: Measure of negative reciprocity",
+                   "σ: Choice sensitivity"]
 
 table1 = DataFrame()
-table1.est_s1 = results_s1
-table1.se_s1 = se_s1
-table1.est_s2 = results_s2
-table1.se_s2 = se_s2
+table1.parameters = parameters_name
+table1.estimates_s1 = round.(results_s1, digits=3)
+table1.standarderr_s1 = round.(se_s1, digits=3)
+table1.zstat_s1 = round.(zvalues_s1, digits=3)
+table1.pval_s1 = round.(pvalues_s1, digits=3)
+table1.estimates_s2 = round.(results_s2, digits=3)
+table1.standarderr_s2 = round.(se_s2, digits=3)
+table1.zstat_s2 = round.(zvalues_s2, digits=3)
+table1.pval_s2 = round.(pvalues_s2, digits=3)
+table1.pval_s1s2 = round.(pvalues_s1s2, digits=3)
+
 CSV.write(raw"../output/table1_julia.csv", table1)
+	
+end
+
+# ╔═╡ 4ee3a2da-1a91-450b-97d8-4108fa08944e
+begin
+
+# We now compute the point estimates for session 1 using JuMP.
+
+# Define a function that takes the variables separately and returns the negative log likelihood. This simplifies the way we can define the variables in the JuMP model later on
+
+function lljump(alpha,beta,gamma,delta,sigma)
+		v = [alpha,beta,gamma,delta,sigma]
+		flljump = loglikeM(v,dt1.choice_x,dt1.self_x,dt1.other_x,dt1.self_y,dt1.other_y,indicators_x,indicators_y)
+	return flljump	
+end
+	
+# Define a new model using JuMP. We use the Ipopt optimizer
+
+model = Model(Ipopt.Optimizer; bridge_constraints = false)
+	
+# You need to clear the julia variables, else you will not be able to run this cell a second time. This is a known problem when using Pluto 
+	
+alpha,beta,gamma,delta,sigma= nothing, nothing, nothing, nothing, nothing
+	
+# Define the variables and the starting values. Our problem is unconstrained.
+
+@variable(model, alpha, start = v0[1])
+@variable(model, beta,  start = v0[2])
+@variable(model, gamma, start = v0[3])
+@variable(model, delta, start = v0[4])
+@variable(model, sigma, start = v0[5])
+@NLobjective(
+        model,
+        Min,
+        lljump(alpha,beta,gamma,delta,sigma) # the function to minimize
+    )
+	
+optimize!(model)
+solution_summary(model)
+end
 
 # ╔═╡ e071b690-3c7d-11ec-0911-bd86ef5668aa
 # We write here the same negative log-likelihood function as before written using a loop over all observations. This is slower but could be interesting to see
@@ -259,6 +382,7 @@ end
 # ╔═╡ Cell order:
 # ╠═7b7526d0-3c9d-11ec-3575-37a66c520386
 # ╠═7b36490e-3c9d-11ec-1ea6-dbf0082362dc
+# ╠═c7fb5b79-7d87-49c5-9459-a360e1057715
 # ╠═a0736b60-3c7d-11ec-1b71-27fd38cbf6e4
 # ╠═e0965590-3c7d-11ec-13b6-a7d15db3fd9f
 # ╠═d8928282-3c85-11ec-00ca-c5215c731b33
@@ -273,5 +397,7 @@ end
 # ╠═e6ab8b50-3cb2-11ec-0b39-e5424b4fdbd2
 # ╠═d554ea40-3cb2-11ec-1642-99c3aadfd3e4
 # ╠═586df880-3cd7-11ec-348c-6133b9ddba73
+# ╠═c32cedfe-daf7-4b0b-81bc-e8028a050294
 # ╠═802c89c0-3d60-11ec-1d69-0dcf94880bf3
+# ╠═4ee3a2da-1a91-450b-97d8-4108fa08944e
 # ╠═e071b690-3c7d-11ec-0911-bd86ef5668aa
